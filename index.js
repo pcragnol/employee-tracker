080d10ede33836ee35dea898b49da26f0cf58d8b
pcragnol <94771135+pcragnol@users.noreply.github.com>
2022-03-15 22:08:52 -0400

    Added schema.sql and seeds.sql and started index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  console.log(`Connected to the ${process.env.DB_NAME} database.`)
+);
