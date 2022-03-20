1a3bbc0fb05f5292764aa846da621dd1adf11163
pcragnol <94771135+pcragnol@users.noreply.github.com>
2022-03-19 22:47:53 -0400

    Added add functionality

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection(
 );
 
 const promptUser = () => {
-  return inquirer.prompt(
+  inquirer.prompt(
     {
       type: 'list',
       name: 'menu',
@@ -30,7 +30,7 @@ const promptUser = () => {
       ]
     }
   )
-  .then((answer) => {
+  .then(answer => {
     switch (answer.menu) {
       case 'View All Departments':
         viewAllDepartments();
@@ -53,24 +53,25 @@ const promptUser = () => {
       case 'Update Employee Role':
         updateEmployeeRole();
         break;
+      default:
+        process.exit();
     }
   });
 };
 
-// const viewAllDepartments = () => {
-//   const sql = 'SELECT * FROM department;';
-//   db.query(sql, (err, result) => {
-//     console.table(result);
-//     promptUser();
-//   });
-// };
-
 const viewAllDepartments = () => {
-  db.query('SELECT * FROM department;');
+  const sql = 'SELECT * FROM department;';
+  db.query(sql, (err, result) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.table(result);
+    promptUser();
+  });
 };
 
 const addDepartment = () => {
-  return inquirer.prompt(
+  inquirer.prompt(
     {
       type: 'input',
       name: 'department',
@@ -84,37 +85,141 @@ const addDepartment = () => {
       }
     }
   )
-  .then((answer) => {
-    const sql = 'INSERT INTO department SET ?';
+  .then(answer => {
+    const sql = 'INSERT INTO department (name) values (?)';
     db.query(sql, answer.department, (err, result) => {
+      if (err) {
+        return console.error(err);
+      }
       console.log(`Added ${answer.department} to departments.`);
       promptUser();
-    })
-  })
+    });
+  });
 };
 
 const viewAllRoles = () => {
   const sql = 'SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;';
   db.query(sql, (err, result) => {
+    if (err) {
+      return console.error(err);
+    }
     console.table(result);
     promptUser();
   });
 };
 
 const addRole = () => {
-  
+  db.query('SELECT name, id AS value FROM department;', (err, result) => {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'department',
+        message: 'Which department would you like to add a role to?',
+        choices: result
+      },
+      {
+        type: 'input',
+        name: 'role',
+        message: 'What role would you like to add?',
+        validate: validateRole => {
+          if (validateRole) {
+            return true;
+          } else {
+            return 'Please enter the role you would like to add.'
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary for the new role?',
+        validate: validateSalary => {
+          if (typeof Number(validateSalary) === 'number' && validateSalary > 0) {
+            return true;
+          } else {
+            return 'Please enter a salary greater than 0.'
+          }
+        }
+      }
+    ])
+    .then(answers => {
+      const sql = 'INSERT INTO role (title, salary, department_id) values (?, ?, ?)';
+      db.query(sql, [answers.role, answers.salary, answers.department], (err, result) => {
+        if (err) {
+          return console.error(err);
+        }
+        console.log(`Added ${answers.role} to roles.`);
+        promptUser();
+      });
+    });
+  });
 };
 
 const viewAllEmployees = () => {
   const sql = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;';
   db.query(sql, (err, result) => {
+    if (err) {
+      return console.error(err);
+    }
     console.table(result);
     promptUser();
   });
 };
 
 const addEmployee = () => {
-  
+  db.query('SELECT title, id AS value FROM role;', (err, result) => {
+    // db.query('SELECT CONCAT(manager.first_name, " ", manager.last_name) AS manager, id as value FROM employee;', (err, result2) => {
+    db.query('SELECT title, id AS value FROM role;', (err, result2) => {
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'firstName',
+          message: "What is the new employee's first name?",
+          validate: validateFirstName => {
+            if (validateFirstName) {
+              return true;
+            } else {
+              return "Please enter the new employee's first name."
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'lastName',
+          message: "What is the new employee's last name?",
+          validate: validateLastName => {
+            if (validateLastName) {
+              return true;
+            } else {
+              return "Please enter the new employee's last name."
+            }
+          }
+        },
+        {
+          type: 'list',
+          name: 'role',
+          message: "What is the new employee's role?",
+          choices: result
+        },
+        {
+          type: 'list',
+          name: 'manager',
+          message: "Who is the new employee's manager?",
+          choices: result2
+        }
+      ])
+      .then(answers => {
+        const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) values (?, ?, ?, ?)';
+        db.query(sql, [answers.firstName, answers.lastName, answers.role, answers.manager], (err, result) => {
+          if (err) {
+            return console.error(err);
+          }
+          console.log(`Added ${answers.firstName} ${answers.lastName} to employees.`);
+          promptUser();
+        });
+      });
+    });
+  });
 };
 
 const updateEmployeeRole = () => {
